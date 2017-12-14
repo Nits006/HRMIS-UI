3bce0a097af1245c72866dc8f9e54dcc155c4456
nits006 <nits.shrivastav@yahoo.com>
2017-12-14 18:28:24 +0530

    commit header hide/show issue

--- app/app.js
+++ app/app.js
@@ -2,17 +2,25 @@ var angularObj = angular.module("hrimsApp", ["ngRoute", "jcs-autoValidate"]);
 
 angularObj.controller('appParentController',function($scope, $location, $timeout){
 
+    $scope.isloggedIn = (localStorage.getItem('is_loggedin') != null && (typeof(localStorage.getItem('is_loggedin')) != 'undefined')?true:false);
+        // if($scope.isloggedIn){
+        //     $location.path('/overview');
+        //     window.location.reload();
+        // }
     $scope.currentPage = $location.path();
     $scope.validateLogin = function(credentials){
         
         $("#loginBtn").button('loading');
         console.log(credentials);
-        $timeout(callAtTimeout, 3000);
+        $timeout(callAtTimeout, 1500);
     }
     
     function callAtTimeout(){
         
         $scope.currentPage = '/overview';
+        localStorage.setItem('is_loggedin', true);
+        $scope.isloggedIn = true;
         $location.path('/overview');
+        window.location.reload();
     }
 });
--- app/components/personal_details/employeeDetailsController.js
+++ app/components/personal_details/employeeDetailsController.js
@@ -1,6 +1,7 @@
 angularObj.controller('employeeDetailsController',function($scope, $location){
-    
+	
+	$scope.isloggedIn = (localStorage.getItem('is_loggedin') != null && (typeof(localStorage.getItem('is_loggedin')) != 'undefined')?true:false);
     $scope.currentPage = $location.path();
     console.log($scope.currentPage);
-    
+
 });
